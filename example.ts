b209abe57addaf319ee5116db2f6c5f3f5b7c3ad
Abdul Al-Hasany <kahhar87@gmail.com>
2024-06-16 18:54:19 +1000

    fix: update deploy actions

--- example.ts
+++ example.ts
@@ -1,8 +1,10 @@
 import { createProgressBar, createHeader } from './index.js';
 
+// Create a header
 createHeader('Header');
-const progress = createProgressBar('Downloading');
 
+// Create a progress bar
+const progress = createProgressBar('Downloading');
 for (let index = 0; index <= 100; index++) {
 	await new Promise((resolve) => {
 		return setTimeout(resolve, 10);
